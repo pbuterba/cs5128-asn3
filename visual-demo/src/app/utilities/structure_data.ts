6bc92ff822eb0426f41ee9172dcce0a58a2a6e67
pbuterba <pbuterba.corp@gmail.com>
2025-04-12 18:32:04 -0400

    Add return for tree function

--- visual-demo/src/app/utilities/structure_data.ts
+++ visual-demo/src/app/utilities/structure_data.ts
@@ -1,8 +1,11 @@
 import { Feature, FeatureTreeNode } from '../types/features';
 
-export function makeTree(dataList: Feature[], fileName: string) {
+export function makeTree(dataList: Feature[], fileName: string): FeatureTreeNode[] {
+   let tree: FeatureTreeNode[] = [];
    dataList.forEach((feature) =>  {
       //Get metadata from file - Preston
       //Check the rest of the list for children, and encorporate into tree structure - Ibrahim
    });
+
+   return tree;
 }
