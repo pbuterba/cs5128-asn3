5741c08617d6c0ee88954b92730718ee6631db0c
pbuterba <pbuterba.corp@gmail.com>
2025-04-12 18:29:42 -0400

    Add basic types and boilerplate for tree function

---
+++ visual-demo/src/app/utilities/structure_data.ts
@@ -0,0 +1,8 @@
+import { Feature, FeatureTreeNode } from '../types/features';
+
+export function makeTree(dataList: Feature[], fileName: string) {
+   dataList.forEach((feature) =>  {
+      //Get metadata from file - Preston
+      //Check the rest of the list for children, and encorporate into tree structure - Ibrahim
+   });
+}
